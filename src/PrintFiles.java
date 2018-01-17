71ce6a4089cb783a542f4b9ea03f1f76c63f93d6
apocarkhesir <adamp218@gmail.com>
2018-01-17 18:29:42 +0100

    Initial commit

---
+++ src/PrintFiles.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.*;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class PrintFiles
+    extends SimpleFileVisitor<Path> {
+    
+    public FileVisitResult visitFile(Path file, Path oldFile) throws IOException {
+        if (!(oldFile.getFileName() == file.getFileName())) {
+            try {
+                Files.copy(oldFile, file, LinkOption.NOFOLLOW_LINKS);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return super.postVisitDirectory(dir, exc);
+    }
+}
